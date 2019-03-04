8bbd0c191dd7883701b02b3649aae26ae0b6fdb7
Nathan Buchar <hello@nathanbuchar.com>
2019-03-04 11:12:14 -0800

    fix: Fix .well-known

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const serve = require('koa-static');
 
 const app = new Koa();
 
-app.use(serve('dist'));
+app.use(serve('dist', {
+  hidden: true, // Serve .well-known
+}));
 
 app.listen(process.env.PORT || 3000);
